a8df43b32b022eaf101d567b434d91ee3b8888ff
Erik Stone <spologas@gmail.com>
2021-09-12 21:48:50 -0700

    added pokedex functions

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch')
+
+
+exports.handler = async function () {
+    const POKE_API = "https://pokeapi.co/api/v2/pokedex/kanto"
+    const response = await fetch(POKE_API)
+    const data = await response.json()
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
+}
